a1e252f88cadab4ff965aec27569820e631c1499
Julian Götting <julian.goetting@gmail.com>
2023-12-13 21:45:02 +0100

    Datenschutz und Anzeigeeinstellungen

--- api_params.js
+++ api_params.js
@@ -2,6 +2,8 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 $( document ).ready(function() {
+    $("#jahr").text(new Date().getFullYear());
+
     let page_title = urlParams.get("titel");
     if (typeof page_title === "string" && page_title.length > 0) {
         $("#headline").text(page_title);
@@ -16,14 +18,14 @@ $( document ).ready(function() {
         container.removeClass("bg-light");
         container.css("background-color", page_boxcolor);
     }
-    let page_impressum = urlParams.get("impressum");
-    if (page_impressum === "false") {
-        $("#impressum").remove();
-    }
-    let page_footer = urlParams.get("footer");
-    if (page_footer === "false") {
-        $("#footer").remove();
-    }
+    // let page_impressum = urlParams.get("impressum");
+    // if (page_impressum === "false") {
+    //     $("#impressum").remove();
+    // }
+    // let page_footer = urlParams.get("footer");
+    // if (page_footer === "false") {
+    //     $("#footer").remove();
+    // }
     let logo_url = urlParams.get("logo_url");
     if (typeof logo_url === "string" && logo_url.length > 0) {
         $('#headline_bar').prepend('<div class="col-sm-auto ps-0 py-3 pe-3" id="headline_user_logo"></div>');
